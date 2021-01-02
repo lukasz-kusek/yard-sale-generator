0b16aef2c3f3d912741be4c547efe2470490d22c
Lukasz Kusek <lukasz@kusek.info>
2021-01-02 18:44:27 +0000

    Added price

--- generate.py
+++ generate.py
@@ -17,34 +17,40 @@ else:
 items = os.listdir(source_dir)
 items_with_details = []
 
-for item in items:
+for item in sorted(items):
     print(source_dir + "/" + item)
     os.mkdir(destination_dir + '/' + item)
 
     details = os.listdir(source_dir + "/" + item)
     photos = []
-    for detail in details:
+    for detail in sorted(details):
         if detail.endswith(".jpg"):
             print(source_dir + "/" + item + "/" + detail)
             copyfile(source_dir + "/" + item + "/" + detail, destination_dir + "/" + item + "/" + detail)
             photos.append(item + "/" + detail)
 
-    for detail in details:
-        if detail == "Opis_exports":
-            print(source_dir + "/" + item + "/" + detail)
-            with open('item.html.mustache', 'r') as item_html_template:
-                with open(destination_dir + '/' + item + '.html', 'w') as item_html:
-                    with open(source_dir + "/" + item + "/" + detail + "/Opis.txt", 'r') as opis:
-                        html = chevron.render(item_html_template, {'name': item, 'description': opis.readlines(), 'photos': photos})
-                        opis.close()
-                    item_html.write(html)
-                    item_html.close()
-                item_html_template.close()
+    with open('item.html.mustache', 'r') as item_html_template:
+        with open(destination_dir + '/' + item + '.html', 'w') as item_html:
+            with open(source_dir + "/" + item + "/Opis_exports/Opis.txt", 'r') as opis_txt:
+                with open(source_dir + "/" + item + "/Cena_exports/Cena.txt", 'r') as cena_txt:
+                    cena = cena_txt.read().strip()
+                    html = chevron.render(item_html_template, {
+                        'name': item,
+                        'price': cena,
+                        'description': opis_txt.readlines(),
+                        'photos': photos})
 
-    items_with_details.append({
-        'name': item,
-        'photo': photos[0]
-    })
+                    items_with_details.append({
+                        'name': item,
+                        'price': cena,
+                        'photo': photos[0]
+                    })
+
+                    cena_txt.close()
+                opis_txt.close()
+            item_html.write(html)
+            item_html.close()
+        item_html_template.close()
 
 with open('index.html.mustache', 'r') as index_html_template:
     with open(destination_dir + '/index.html', 'w') as index_html:
