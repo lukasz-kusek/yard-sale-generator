d6cea4bab2c0ce46dce0c84573b2c184bfd01e8e
Lukasz Kusek <lukasz@kusek.info>
2021-01-02 17:32:03 +0000

    Initial commit

---
+++ generate.py
@@ -0,0 +1,49 @@
+import os
+from shutil import copyfile, rmtree
+import chevron
+
+source_dir = '../yard-sale'
+destination_dir = '../yard-sale-html'
+
+if os.path.isdir(destination_dir):
+    rmtree(destination_dir)
+os.mkdir(destination_dir)
+
+items = os.listdir(source_dir)
+items_with_details = []
+
+for item in items:
+    print(source_dir + "/" + item)
+    os.mkdir(destination_dir + '/' + item)
+
+    details = os.listdir(source_dir + "/" + item)
+    photos = []
+    for detail in details:
+        if detail.endswith(".jpg"):
+            print(source_dir + "/" + item + "/" + detail)
+            copyfile(source_dir + "/" + item + "/" + detail, destination_dir + "/" + item + "/" + detail)
+            photos.append(item + "/" + detail)
+
+    for detail in details:
+        if detail == "Opis_exports":
+            print(source_dir + "/" + item + "/" + detail)
+            with open('item.html.mustache', 'r') as item_html_template:
+                with open(destination_dir + '/' + item + '.html', 'w') as item_html:
+                    with open(source_dir + "/" + item + "/" + detail + "/Opis.txt", 'r') as opis:
+                        html = chevron.render(item_html_template, {'name': item, 'description': opis.readlines(), 'photos': photos})
+                        opis.close()
+                    item_html.write(html)
+                    item_html.close()
+                item_html_template.close()
+
+    items_with_details.append({
+        'name': item,
+        'photo': photos[0]
+    })
+
+with open('index.html.mustache', 'r') as index_html_template:
+    with open(destination_dir + '/index.html', 'w') as index_html:
+        html = chevron.render(index_html_template, {'items': items_with_details})
+        index_html.write(html)
+        index_html.close()
+    index_html_template.close()
